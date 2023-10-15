e61e194a3ae2edd959d219c9475929fbd2ecba48
Francis Mercier <arkilar_23@hotmail.com>
2023-10-14 22:07:13 -0400

    mettre clean les classe creation compte

--- app/src/main/java/com/example/thewitcher/CreationActivity.java
+++ app/src/main/java/com/example/thewitcher/CreationActivity.java
@@ -57,16 +57,116 @@ public class CreationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creation);
-
+        
+        initializeViews();
+        setupOnClickListerners();
         //Initialize database
         WitcherRoomDatabase database = WitcherRoomDatabase.getDatabase(this);
-
         //Initialize baseRepository
         baseRepository = new BaseRepository(getApplication());
-
         //Set activity title
         setTitle("New Character");
 
+    }
+
+    private void setupOnClickListerners() {
+        btnSave.setOnClickListener(v -> saveCharacter());
+
+        ActivityResultLauncher<Integer> raceResultLauncher = registerForActivityResult(new RaceResultContract(), this::onRaceActivityResult);
+        btnRace.setOnClickListener(v -> raceResultLauncher.launch(1));
+
+        ActivityResultLauncher<Integer> classResultLauncher = registerForActivityResult(new ClassResultContract(), this::onClassActivityResult);
+        btnClass.setOnClickListener(v -> classResultLauncher.launch(1));
+
+        ActivityResultLauncher<Integer> armorResultLauncher = registerForActivityResult(new ArmorResultContract(), this::onArmorActivityResult);
+        btnArmor.setOnClickListener(v -> armorResultLauncher.launch(1));
+
+        ActivityResultLauncher<Integer> weaponResultLauncher = registerForActivityResult(new WeaponResultContract(), this::onWeaponActivityResult);
+        btnWeapon.setOnClickListener(v -> weaponResultLauncher.launch(1));
+    }
+
+    private void onRaceActivityResult(Integer result) {
+        if (result != null && result != -1) {
+            LiveData<Race> liveRace = baseRepository.findRaceById(result);
+            liveRace.observe(getLifecycleOwner(), selectedRace -> {
+                race = selectedRace;
+                btnRace.setText(race.getName());
+            });
+        }
+    }
+
+    private void saveCharacter() {
+        String name = txtNameInput.getText().toString();
+        int age = Integer.parseInt(txtAge.getText().toString());
+        String background = txtBackground.getText().toString();
+
+        CompletableFuture.supplyAsync(() -> {
+            // Insert personnage and get its ID
+            int personnageId = baseRepository.insertPersonnage(
+                    new Personnage(name, age, race.getRaceId(), classe.getClassId(),
+                            armor.getArmorId(), weapon.getWeaponId(),
+                            background));
+            return personnageId;
+        }).thenAccept(personnageId -> {
+            // Use the personnageId to insert owned skills
+            for (Map.Entry<Skill, Integer> entry : skillLevels.entrySet()) {
+                baseRepository.insertOwnedSkill(new OwnedSkill(entry.getKey().getSkillId(),
+                        personnageId,
+                        entry.getValue()));
+            }
+        });
+        Intent intent = new Intent(CreationActivity.this, AccountActivity.class);
+        startActivity(intent);
+    }
+
+    private void onClassActivityResult(Integer result) {
+        if (result != null && result != -1) {
+            LiveData<Classe> liveClasse = baseRepository.findClasseById(result);
+            liveClasse.observe(getLifecycleOwner(), selectedClasse -> {
+                classe = selectedClasse;
+                btnClass.setText(classe.getName());
+                RecyclerView listCreationSkill = findViewById(R.id.listCreationSkills);
+                listCreationSkill.setLayoutManager(new LinearLayoutManager(getApplication()));
+                viewModel = new SkillViewModel(getApplication(), classe.getClassId());
+                adapter = new SkillSelectionAdapter(getApplicationContext(), skillArray);
+                adapter.setOnTotalSkillLevelChangeListener(totalSkillLevel -> {
+                    int availableSkillPoints = 20 - totalSkillLevel;
+                    if (availableSkillPoints < 0) availableSkillPoints = 0;
+                    TextView txtSkillPoints = findViewById(R.id.txtSkillPoints);
+                    txtSkillPoints.setText(String.valueOf(availableSkillPoints));
+                    skillLevels = adapter.getSkillLevels();
+                });
+                listCreationSkill.setAdapter(adapter);
+                viewModel.getAllSkills().observe(getLifecycleOwner(), skill -> {
+                    skillArray.clear();
+                    skillArray.addAll(skill);
+                    adapter.notifyDataSetChanged();
+                });
+            });
+        }
+    }
+
+    private void onArmorActivityResult(Integer result) {
+        if (result != null && result != -1) {
+            LiveData<Armor> liveArmor = baseRepository.findArmorById(result);
+            liveArmor.observe(getLifecycleOwner(), selectedArmor -> {
+                armor = selectedArmor;
+                btnArmor.setText(armor.getName());
+            });
+        }
+    }
+
+    private void onWeaponActivityResult(Integer result) {
+        if (result != null && result != -1) {
+            LiveData<Weapon> liveWeapon = baseRepository.findWeaponById(result);
+            liveWeapon.observe(getLifecycleOwner(), selectedWeapon -> {
+                weapon = selectedWeapon;
+                btnWeapon.setText(weapon.getName());
+            });
+        }
+    }
+
+    private void initializeViews() {
         txtNameInput = findViewById(R.id.txtNameInput);
         txtAge = findViewById(R.id.txtAge);
         txtBackground = findViewById(R.id.txtBackground);
@@ -75,142 +175,6 @@ public class CreationActivity extends AppCompatActivity {
         btnClass = findViewById(R.id.btnClass);
         btnArmor = findViewById(R.id.btnArmor);
         btnWeapon = findViewById(R.id.btnWeapon);
-
-        //Create onClickListener for btnSave to save the new informations in the database
-        btnSave.setOnClickListener(v -> {
-            String name = txtNameInput.getText().toString();
-            int age = Integer.parseInt(txtAge.getText().toString());
-            String background = txtBackground.getText().toString();
-
-            CompletableFuture.supplyAsync(() -> {
-                // Insert personnage and get its ID
-                int personnageId = baseRepository.insertPersonnage(
-                        new Personnage(name, age, race.getRaceId(), classe.getClassId(),
-                                armor.getArmorId(), weapon.getWeaponId(),
-                                background));
-                return personnageId;
-            }).thenAccept(personnageId -> {
-                // Use the personnageId to insert owned skills
-                for (Map.Entry<Skill, Integer> entry : skillLevels.entrySet()) {
-                    baseRepository.insertOwnedSkill(new OwnedSkill(entry.getKey().getSkillId(),
-                            personnageId,
-                            entry.getValue()));
-                }
-            });
-            Intent intent = new Intent(CreationActivity.this, AccountActivity.class);
-            startActivity(intent);
-        });
-
-
-        //Create ActivityResultLauncher for btnRace
-        ActivityResultLauncher<Integer> raceResultLauncher =
-                registerForActivityResult(new RaceResultContract(), new ActivityResultCallback<Integer>() {
-                    @Override
-                    public void onActivityResult(Integer result) {
-                        if (result != null && result != -1) {
-                            LiveData<Race> liveRace = baseRepository.findRaceById(result);
-                            liveRace.observe(getLifecycleOwner(), selectedRace -> {
-                                race = selectedRace;
-                                btnRace.setText(race.getName());
-                            });
-
-                        }
-                    }
-                });
-
-        //Create ActivityResultLauncher for btnClasse
-        ActivityResultLauncher<Integer> classeResultLauncher =
-                registerForActivityResult(new ClassResultContract(), new ActivityResultCallback<Integer>() {
-                    @Override
-                    public void onActivityResult(Integer result) {
-                        if (result != null && result != -1) {
-                            LiveData<Classe> liveClasse = baseRepository.findClasseById(result);
-                            liveClasse.observe(getLifecycleOwner(), selectedClasse -> {
-                                classe = selectedClasse;
-                                btnClass.setText(classe.getName());
-
-                                RecyclerView listCreationSkill = findViewById(R.id.listCreationSkills);
-                                listCreationSkill.setLayoutManager(new LinearLayoutManager(getApplication()));
-
-                                viewModel = new SkillViewModel(getApplication(), classe.getClassId());
-
-                                Log.d("data", "skillArray: " + skillArray.size());
-                                //Send data to adapter
-                                adapter = new SkillSelectionAdapter(getApplicationContext(), skillArray);
-                                adapter.setOnTotalSkillLevelChangeListener(new SkillSelectionAdapter.OnTotalSkillLevelChangeListener() {
-                                    @Override
-                                    public void onTotalSkillLevelChange(int totalSkillLevel) {
-                                        int availableSkillPoints = 20 - totalSkillLevel;
-                                        if(availableSkillPoints < 0) availableSkillPoints = 0;
-                                        TextView txtSkillPoints = findViewById(R.id.txtSkillPoints);
-                                        txtSkillPoints.setText(String.valueOf(availableSkillPoints));
-
-                                        skillLevels = adapter.getSkillLevels();
-                                    }
-                                });
-
-                                listCreationSkill.setAdapter(adapter);
-
-                                viewModel.getAllSkills().observe(getLifecycleOwner(), skill -> {
-                                    skillArray.clear();
-                                    skillArray.addAll(skill);
-                                    adapter.notifyDataSetChanged();
-                                });
-                            });
-
-                        }
-                    }
-                });
-
-        //Create ActivityResultLauncher for btnArmor
-        ActivityResultLauncher<Integer> armorResultLauncher =
-                registerForActivityResult(new ArmorResultContract(), new ActivityResultCallback<Integer>() {
-                    @Override
-                    public void onActivityResult(Integer result) {
-                        if (result != null && result != -1) {
-                            LiveData<Armor> liveArmor = baseRepository.findArmorById(result);
-                            liveArmor.observe(getLifecycleOwner(), selectedClasse -> {
-                                armor = selectedClasse;
-                                btnArmor.setText(armor.getName());
-                            });
-                        }
-                    }
-                });
-
-        //Create ActivityResultLauncher for btnWeapon
-        ActivityResultLauncher<Integer> weaponResultLauncher =
-                registerForActivityResult(new WeaponResultContract(), new ActivityResultCallback<Integer>() {
-                    @Override
-                    public void onActivityResult(Integer result) {
-                        if (result != null && result != -1) {
-                            LiveData<Weapon> liveWeapon = baseRepository.findWeaponById(result);
-                            liveWeapon.observe(getLifecycleOwner(), selectedClasse -> {
-                                weapon = selectedClasse;
-                                btnWeapon.setText(weapon.getName());
-                            });
-                        }
-                    }
-                });
-
-        //Create onClickListener for btnRace sending the user to ChooseRaceActivity
-        btnRace.setOnClickListener(v -> {
-            raceResultLauncher.launch(1);
-        });
-
-        //Create onClickListener for btnClass sending the user to ChooseClassActivity
-        btnClass.setOnClickListener(v -> {
-            classeResultLauncher.launch(1);
-        });
-
-        //Create onClickListener for btnArmor sending the user to ChooseArmorActivity
-        btnArmor.setOnClickListener(v -> {
-            armorResultLauncher.launch(1);
-        });
-
-        //Create onClickListener for btnWeapon sending the user to ChooseWeaponActivity
-        btnWeapon.setOnClickListener(v -> {
-            weaponResultLauncher.launch(1);
-        });
     }
 
     private LifecycleOwner getLifecycleOwner() {
